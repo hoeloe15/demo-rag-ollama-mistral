3de9264197764260e7517fa7d55fd6f09475e3bc
mark <mark.9696@hotmail.com>
2024-07-13 09:48:50 +0200

    updated with openai call

--- backend.py
+++ backend.py
@@ -1,15 +1,22 @@
 import os
 import pickle
-from flask import Flask, request, jsonify
+import warnings
+from flask import Flask, request, jsonify, render_template
+from dotenv import load_dotenv
 from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Flask setup
 app = Flask(__name__)
@@ -17,7 +24,12 @@ app = Flask(__name__)
 local_path = "data/ISO 27001.pdf"
 chunks_file = "data/chunks.pkl"
 vector_db_path = "data/vector_db"
-embedding_function = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+if not openai_api_key:
+    raise ValueError("OPENAI_API_KEY environment variable not set")
+
+embedding_function = OpenAIEmbeddings(model="text-embedding-ada-002", api_key=openai_api_key)
 
 # Initialize global variables
 chain = None
@@ -49,9 +61,8 @@ def initialize():
             else:
                 raise FileNotFoundError("PDF file not found.")
 
-        local_model = "mistral"
         print("Loading LLM model...")
-        llm = ChatOllama(model=local_model)
+        llm = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo")
 
         global vector_db
         if os.path.exists(vector_db_path):
@@ -99,6 +110,10 @@ def initialize():
 
 # Initialize the model when the script starts
 initialize()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/ask', methods=['POST'])
 def ask():
