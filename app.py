0199b1cf7903dc88d81925de8ee23a686e54f05a
hoeloe <mark.9696@hotmail.com>
2024-07-17 16:03:07 +0200

    some change in the frontend app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from styles import apply_custom_css, apply_custom_js
 from chat_handler import init_session_state, display_chat_history, handle_user_input, clear_chat_history
 
 def main():
-    hosted_local = True if len(sys.argv) > 1 and sys.argv[1] == "local" else False
-    backend_url = st.secrets.BACKEND_URL.local_url if hosted_local else st.secrets.BACKEND_URL.backend_url
+    # hosted_local = True if len(sys.argv) > 1 and sys.argv[1] == "local" else False
+    # backend_url = st.secrets.BACKEND_URL.local_url if hosted_local else st.secrets.BACKEND_URL.backend_url
+    backend_url = st.secrets.BACKEND_URL.backend_url
 
     apply_custom_css()
     apply_custom_js()
